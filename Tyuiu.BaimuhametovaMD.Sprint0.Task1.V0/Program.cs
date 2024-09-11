53627405845d507ae50d70eb83e891e7eed1967e
BaimuhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-09-11 16:54:55 +0500

    task1

---
+++ Tyuiu.BaimuhametovaMD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.BaimuhametovaMD.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Мадина";
+            int age = 21;
+            Console.WriteLine("Здравствуйте, "+name+", мне "+age+" лет!");
+            Console.ReadKey();
+        }
+    }
+}
