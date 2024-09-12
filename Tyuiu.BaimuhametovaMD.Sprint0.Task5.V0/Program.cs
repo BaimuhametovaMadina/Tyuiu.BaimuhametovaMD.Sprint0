50549bb653fca3736b5012bb3f840dc290ef37dc
BaimuhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-09-12 23:05:15 +0500

    task 5-2

--- Tyuiu.BaimuhametovaMD.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.BaimuhametovaMD.Sprint0.Task5.V0/Program.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.BaimuhametovaMD.Sprint0.Task5.V0
             Console.WriteLine(DataService.Addition(1, 5));
             Console.WriteLine(DataService.Substraction(15, 5));
             Console.WriteLine(DataService.Multiplication(10, 10));
-            Console.WriteLine(DataService.Division(5, 5));
+            Console.WriteLine(DataService.Division(5, 0));
             Console.ReadKey();
         }
     }
