f90fab64809fb82a1f32adc0eccc04e284fa15c0
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-09-13 15:15:47 +0500

    task5

--- Tyuiu.BaimuhametovaMD.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.BaimuhametovaMD.Sprint0.Task5.V0/Program.cs
@@ -1,16 +1,1 @@
-﻿using Tyuiu.BaimuhametovaMD.Sprint0.Task5.V0.Lib;
-
-namespace Tyuiu.BaimuhametovaMD.Sprint0.Task5.V0
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
-            Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
-            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
-            Console.WriteLine("A / B = " + DataService.Division(5, 0));
-            Console.ReadKey();
-        }
-    }
-}
+﻿
