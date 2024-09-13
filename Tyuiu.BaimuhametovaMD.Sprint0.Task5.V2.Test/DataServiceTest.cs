83c7128755223066ebcff3b34f962e49dc14823d
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-09-13 14:52:48 +0500

    task5

---
+++ Tyuiu.BaimuhametovaMD.Sprint0.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+using Tyuiu.BaimuhametovaMD.Sprint0.Task5.V2.Lib;
+
+namespace Tyuiu.BaimuhametovaMD.Sprint0.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
+
